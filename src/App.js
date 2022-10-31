a8b7bb130aaa9942cfb3651f5c8dec16249c8d51
Niloofar <niloofar.navaieelavasani@gmail.com>
2022-10-31 11:06:45 +0100

    Added the footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+
+      <footer>
+        This project was coded by{" "}
+        <a href="" target="_blank">
+          Niloofar Navaiee
+        </a>{" "}
+        and is an open-source on{" "}
+        <a href="" target="_blank">
+          Github
+        </a>
+      </footer>
+    </div>
+  );
 }
