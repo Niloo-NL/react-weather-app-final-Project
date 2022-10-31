f53e46c131e3fd0fd72adb07a107fa3b4bce1fdd
Niloofar <niloofar.navaieelavasani@gmail.com>
2022-10-31 11:20:25 +0100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
+      <div className="container">
+        <h1>Weather App</h1>
 
-      <footer>
-        This project was coded by{" "}
-        <a href="" target="_blank">
-          Niloofar Navaiee
-        </a>{" "}
-        and is an open-source on{" "}
-        <a href="" target="_blank">
-          Github
-        </a>
-      </footer>
+        <footer>
+          This project was coded by{" "}
+          <a href="" target="_blank">
+            Niloofar Navaiee
+          </a>{" "}
+          and is an open-source on{" "}
+          <a href="" target="_blank">
+            Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
