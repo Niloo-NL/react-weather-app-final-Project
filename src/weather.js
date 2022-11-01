50bcd0ba7031b5f49703c62fe152ec7526f66e3d
Niloofar <niloofar.navaieelavasani@gmail.com>
2022-11-01 14:35:29 +0100

    Added basic styling

--- src/weather.js
+++ src/weather.js
@@ -6,27 +6,32 @@ export default function Weather() {
     <div className="weather">
       <form>
         <div className="row">
-          <div className="col-6">
+          <div className="col-4">
             <input
               type="search"
-              placeholder="City Name"
+              placeholder="Type a City.."
               className="form-control"
+              autoFocus="on"
             />
           </div>
-          <div className="col">
-            <input type="submit" value="search" className="btn btn-primary" />
+          <div className="col-2">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
       </form>
       <h1>Berlin</h1>
       <ul>
         <li> Wednesday 07:00 </li>
-        <li>Mostly Cloudy</li>{" "}
+        <li> Mostly Cloudy</li>{" "}
       </ul>
       <div className="row">
-        <div className="col-6">
+        <div className="col-8">
           <div className="row">
-            <div className="col-2">
+            <div className="col-3">
               {" "}
               <img
                 src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
@@ -35,11 +40,13 @@ export default function Weather() {
               />
             </div>
             <div className="col-6">
-              <span className="current-temp"> 29°C</span>
+              <span className="current-temp">
+                29<span className="unit">°C</span>
+              </span>
             </div>
           </div>
         </div>
-        <div className="col-6">
+        <div className="col-3">
           <ul>
             <li>Precipitaton: 2%</li>
             <li> Humidity: 50%</li>
